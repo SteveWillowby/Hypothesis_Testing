1f58fb2275156aed50a4bda1449d17dd66616f22
Steve Willowby <justus.i.hibshman@gmail.com>
2020-12-17 16:49:13 -0500

    More bound code. Binomial for binomial is not convex. :(

--- my_bound_funcs.py
+++ my_bound_funcs.py
@@ -49,5 +49,53 @@ def memoized_my_universal_bound_on_binomial(S, C, coin_prob, P_N=0.5):
     __memoized_my_universal_bound_values__[key] = value
     return value
 
+__memoized_my_binomial_bound_on_values__ = {}
+
+# Returns the confidence that p_given_p >= b
+def __my_binomial_bound_on_binomial_helper_helper__(b, p, S):
+    total = bigfloat.BigFloat(0.0)
+    for C in range(0, int(S * p) + 1):
+        p_given_p = bigfloat_prob_of_count_given_p(C, p, S)
+        if p_given_p >= b:
+            break
+        total += p_given_p
+    for i in range(0, int(S) - int(S*p)):
+        C = int(S) - i
+        p_given_p = bigfloat_prob_of_count_given_p(C, p, S)
+        if p_given_p >= b:
+            break
+        total += p_given_p
+    return 1.0 - total
+
+def __my_binomial_bound_on_binomial_helper__(P_X_given_N, b, S, P_N):
+    func = (lambda x: lambda y: __my_binomial_bound_on_binomial_helper_helper__(x[0], y, x[1]))((b, S))
+    (best_p, lowest_outer_confidence) = binary_min_finder(func, 0.0, 1.0)
+    print("Best p: %f" % best_p)
+
+    P_not_N = 1.0 - P_N
+    chance_correct = lowest_outer_confidence
+    if_correct = (P_X_given_N * P_N) / (P_X_given_N * P_N + b * P_not_N)
+    alpha = chance_correct * if_correct + (1.0 - chance_correct) * P_N
+
+    return ((1.0 - alpha) * P_N) / (alpha * P_not_N)
+
+# Makes strong convexity assumption on both p and b.
 def my_binomial_bound_on_binomial(S, C, coin_prob, P_N=0.5):
-    pass
+    P_X_given_N = bigfloat_prob_of_count_given_p(C, coin_prob, S)
+    func = (lambda x: lambda y: -1.0 * __my_binomial_bound_on_binomial_helper__(x[0], y, x[1], x[2]))((P_X_given_N, S, P_N))
+    (best_b, neg_evidence) = binary_min_finder(func, 0.0, 1.0)
+    print("Best b: %f" % best_b)
+    return -1.0 * neg_evidence
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    outer_values = []
+    prob_values = []
+    for numerator in range(0, 10000):
+        prob_value = numerator / 10000.0
+        outer_value = __my_binomial_bound_on_binomial_helper_helper__(0.1, prob_value, 10.0)
+        outer_values.append(outer_value)
+        prob_values.append(prob_value)
+
+    plt.plot(prob_values, outer_values)
+    plt.show()
