1beb58d620724adfdb33ef96a39e340c0f6f8750
Steve Willowby <justus.i.hibshman@gmail.com>
2021-01-14 14:32:10 -0500

    Fixed a bug and added support for scatter on top of surface.

--- visualize_my_bound.py
+++ visualize_my_bound.py
@@ -11,7 +11,7 @@ from my_bound_funcs import my_universal_bound_on_binomial
 # marked_points -- a list of (C, P_N) points to highlight on the 3d contour
 # num_priors -- the number of distinct prior values for which to compute the surface
 def visualize_my_universal_bound_on_binomial(num_flips, coin_prob=0.5, marked_points=[], num_priors=1001):
-    start_P_N = bigfloat.pow(2.0, -50)
+    start_P_N = bigfloat.pow(2.0, -30)
     end_P_N = 1.0 - start_P_N
     P_N_increment = (end_P_N - start_P_N) / (num_priors - 1)
     P_N = start_P_N
@@ -50,17 +50,18 @@ def visualize_my_universal_bound_on_binomial(num_flips, coin_prob=0.5, marked_po
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-
-    surf = ax.plot_surface(C_vals_2d, P_N_vals_2d, evidence_vals, cmap=cm.coolwarm,
-               linewidth=0, antialiased=False)
+    # plt.hold(True)  # Necessary to have both the scatter points and the surface.
 
     marked_points = [(float(a), float(b)) for (a, b) in marked_points]
-    marked_points = [(a, b, my_universal_bound_on_binomial(num_flips, a, coin_prob, b) \
+    marked_points = [(a, b, float(my_universal_bound_on_binomial(num_flips, a, coin_prob, b))) \
         for (a, b) in marked_points]
     marked_x = [a for (a, _, _) in marked_points]
     marked_y = [b for (_, b, _) in marked_points]
     marked_z = [c for (_, _, c) in marked_points]
-    ax.scatter(marked_x, marked_y, marked_z, 'green')
+    points_plot = ax.scatter(marked_x, marked_y, marked_z, c="green", alpha=1)
+
+    surface_plot = ax.plot_surface(C_vals_2d, P_N_vals_2d, evidence_vals, \
+               linewidth=0, antialiased=False, cmap=plt.get_cmap('RdBu_r'), alpha=0.5)  # 'Blues'
 
     plt.suptitle("Log_2 of Strength of Evidence Against Null (N)")
     plt.xlabel("Number of Heads H")
@@ -73,6 +74,6 @@ if __name__ == "__main__":
     bf_context = bigfloat.Context(precision=2000, emax=100000, emin=-100000)
     bigfloat.setcontext(bf_context)
 
-    visualize_my_universal_bound_on_binomial(60, \
+    visualize_my_universal_bound_on_binomial(600, \
         coin_prob=(1.0 / bigfloat.BigFloat(6.0)), \
-        marked_points=[], num_priors=301)
+        marked_points=[(113, 0.5)], num_priors=101)
