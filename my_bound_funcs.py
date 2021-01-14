39df98db1ba93d6a60078028d52734133b1329f7
Steve Willowby <justus.i.hibshman@gmail.com>
2021-01-14 13:17:01 -0500

    Added code for directly computing the ideal m. Also, some new visuals.

--- my_bound_funcs.py
+++ my_bound_funcs.py
@@ -1,51 +1,59 @@
 from optimizing import *
 from comb_and_prob_funcs import bigfloat_prob_of_count_given_p
 
-def __best_m_and_evidence_strength_helper__(P_X_given_N, P_N, n, m):
+def __best_m__(P_X_given_N, P_N, n):
     assert P_N != 1.0
     assert P_N != 0.0
     P_not_N = 1.0 - P_N
-    chance_correct = (P_not_N * (1.0 - (n - 1) / m))
-    if_correct = (P_X_given_N * P_N) / (P_X_given_N * P_N + (1.0 / m) * P_not_N)
-    alpha = chance_correct * if_correct + (1.0 - chance_correct) * P_N
-    return ((1.0 - alpha) * P_N) / (alpha * P_not_N)
+
+    best_m_numerator = 1.0 + bigfloat.sqrt(1.0 + (P_not_N / P_N) * (1.0 + 1.0 / (P_X_given_N * P_N * (n - 1))))
+    best_m_denominator = P_X_given_N + 1.0 / (P_N * (n - 1))
+    return bigfloat.max(n, best_m_numerator / best_m_denominator)
+
+def __universal_bound__(P_X_given_N, P_N, n):
+    assert P_N != 1.0
+    assert P_N != 0.0
+    P_not_N = 1.0 - P_N
+    best_m = __best_m__(P_X_given_N, P_N, n)
+
+    chance_correct = (P_not_N * (1.0 - (n - 1) / best_m))
+    if_correct = (P_X_given_N * P_N) / (P_X_given_N * P_N + (1.0 / best_m) * P_not_N)
+    p_star = chance_correct * if_correct + (1.0 - chance_correct) * P_N
+    return ((1.0 - p_star) * P_N) / (p_star * P_not_N)
 
 # S -- number of flips
 # C -- number of flips that landed heads
 # p_null -- the proportion of flips that the null hypothesis expects to be heads
-def my_universal_bound_on_binomial(S, C, coin_prob, P_N=0.5):
+# P_N == prior probability that the null is true
+def my_universal_bound_on_binomial(S, C, p_null, P_N=0.5):
     n = S + 1
-    P_X_given_N = bigfloat_prob_of_count_given_p(C, coin_prob, S)
-    # if P_X_given_N >= 1.0 / n:
-    #     return 1.0
-    func = (lambda x: lambda y: __best_m_and_evidence_strength_helper__(x[0], x[1], x[2], y))((P_X_given_N, P_N, n))
-    # return (best_arg, best_func)
-    (best_m, best_func_val) = max_finder_low_bound_only(func, S)
+    P_X_given_N = bigfloat_prob_of_count_given_p(C, p_null, S)
 
-    return best_func_val
+    return __universal_bound__(P_X_given_N, P_N, n)
 
 __memoized_my_universal_bound_values__ = {}
 
 # S -- number of flips
 # C -- number of flips that landed heads
 # p_null -- the proportion of flips that the null hypothesis expects to be heads
-def memoized_my_universal_bound_on_binomial(S, C, coin_prob, P_N=0.5):
+# P_N == prior probability that the null is true
+def memoized_my_universal_bound_on_binomial(S, C, p_null, P_N=0.5):
     assert int(C) == C
     C = int(C)
     assert int(S) == S
     S = int(S)
     assert float(P_N) == P_N
     P_N = float(P_N)
-    assert float(coin_prob) == coin_prob
-    coin_prob = float(coin_prob)
+    assert float(p_null) == p_null
+    p_null = float(p_null)
 
     global __memoized_my_universal_bound_values__
 
-    key = (S, C, coin_prob, P_N)
+    key = (S, C, p_null, P_N)
     if key in __memoized_my_universal_bound_values__:
         return __memoized_my_universal_bound_values__[key]
 
-    value = my_universal_bound_on_binomial(S, C, coin_prob, P_N)
+    value = my_universal_bound_on_binomial(S, C, p_null, P_N)
     __memoized_my_universal_bound_values__[key] = value
     return value
 
---
+++ visualize_my_bound.py
@@ -0,0 +1,78 @@
+import bigfloat
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from my_bound_funcs import my_universal_bound_on_binomial
+
+
+# num_flips -- the total number of flips of the coin (not the number of heads)
+# coin_prob -- the coin's probability
+# marked_points -- a list of (C, P_N) points to highlight on the 3d contour
+# num_priors -- the number of distinct prior values for which to compute the surface
+def visualize_my_universal_bound_on_binomial(num_flips, coin_prob=0.5, marked_points=[], num_priors=1001):
+    start_P_N = bigfloat.pow(2.0, -50)
+    end_P_N = 1.0 - start_P_N
+    P_N_increment = (end_P_N - start_P_N) / (num_priors - 1)
+    P_N = start_P_N
+    
+    start_C = 0
+    end_C = num_flips
+    C_increment = 1
+    C = start_C
+
+    evidence_vals = []
+    C_vals = []
+
+
+    while C <= end_C:
+        C_vals.append(float(C))
+        evidence_vals.append([])
+
+        P_N_vals = []
+        P_N = start_P_N
+        while P_N <= end_P_N:
+            P_N_vals.append(float(P_N))
+
+            evidence_bound = my_universal_bound_on_binomial(num_flips, C, coin_prob, P_N)
+            evidence_vals[-1].append(float(bigfloat.log2(evidence_bound)))
+
+            P_N += P_N_increment
+        C += C_increment
+
+    # Plotting Data
+
+    # Have the "major" axis be C_vals and the "secondary" axis be P_N_vals
+    P_N_vals_2d = np.array([P_N_vals for _ in C_vals])
+    C_vals_2d = np.array([[C_vals[i] for _ in P_N_vals] for i in range(0, len(C_vals))])
+
+    evidence_vals = np.array(evidence_vals)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    surf = ax.plot_surface(C_vals_2d, P_N_vals_2d, evidence_vals, cmap=cm.coolwarm,
+               linewidth=0, antialiased=False)
+
+    marked_points = [(float(a), float(b)) for (a, b) in marked_points]
+    marked_points = [(a, b, my_universal_bound_on_binomial(num_flips, a, coin_prob, b) \
+        for (a, b) in marked_points]
+    marked_x = [a for (a, _, _) in marked_points]
+    marked_y = [b for (_, b, _) in marked_points]
+    marked_z = [c for (_, _, c) in marked_points]
+    ax.scatter(marked_x, marked_y, marked_z, 'green')
+
+    plt.suptitle("Log_2 of Strength of Evidence Against Null (N)")
+    plt.xlabel("Number of Heads H")
+    plt.ylabel("Prior Value, P(N)")
+    plt.title("Log_2( P(H | not-N) / P(H | N) )")
+
+    plt.show()
+
+if __name__ == "__main__":
+    bf_context = bigfloat.Context(precision=2000, emax=100000, emin=-100000)
+    bigfloat.setcontext(bf_context)
+
+    visualize_my_universal_bound_on_binomial(60, \
+        coin_prob=(1.0 / bigfloat.BigFloat(6.0)), \
+        marked_points=[], num_priors=301)
